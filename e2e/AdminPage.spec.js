0c6874d5e9dcd960eddcc17e23e29c41b08a31bf
akash <akashb@heaptrace.com>
2025-10-27 11:23:53 +0530

    Bug: resolved the error of LoginPage

--- e2e/AdminPage.spec.js
+++ e2e/AdminPage.spec.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 const PageManager = require('../pages/PageManager');
 
-test('Dashboard test using saved session', async ({ browser }) => {
+test('AdminPage test using saved session', async ({ browser }) => {
   const context = await browser.newContext({ storageState: 'storageState.json' });
   const page = await context.newPage();
     const pageManager = new PageManager(page);
